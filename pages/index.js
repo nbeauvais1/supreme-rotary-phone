91dbc4df477b8dcfefc99868ddbaffa67c3bc94d
Natalie Beauvais <beauvais.natalie7@gmail.com>
2022-04-22 23:20:32 -0600

    So many errors!!!!

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
     <>
       <Head>
         <meta charset="UTF-8"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Plant Store</title>
       </Head>
